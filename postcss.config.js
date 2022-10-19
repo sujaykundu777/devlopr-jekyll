a51e9410e78dbd0d1de67679e42001fb3f964dd5
Sujay Kundu <sujaykundu777@gmail.com>
2022-10-20 02:03:44 +0530

    update homepage

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-// postcss.config.js
-
-module.exports = {
-    plugins: [
-      require('postcss-import'),
-      require('tailwindcss'),
-      require('autoprefixer'),
-    ]
-  }
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./**/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
