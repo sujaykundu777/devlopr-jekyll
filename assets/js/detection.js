dee26bead568d7681942e20e0bcbcf2200561c43
Tello- <tellovisionstudio@gmail.com>
2023-08-17 20:27:53 -0700

    trying based on api owner inspected source

--- assets/js/detection.js
+++ assets/js/detection.js
@@ -1,38 +1,35 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById('detection-form');
-    const resultContainer = document.getElementById('result-container');
+    const form = document.getElementById('ai-detection-form');
+    const resultContainer = document.getElementById('ai-result-container');
+    const apiUrl = 'https://atrium-prod-api.optic.xyz/aion/ai-generated/reports';
+    const apiKey = '9b23c426-2e37-4edb-b6c3-162acbca0d1c'; // Replace with your actual API key
 
     form.addEventListener('submit', async function (event) {
         event.preventDefault();
 
-        const imageUrl = document.getElementById('image-url').value;
-
-        // API endpoint and headers
-        const apiUrl = 'https://atrium-prod-api.optic.xyz/aion/ai-generated/reports';
-        const apiKey = '9b23c426-2e37-4edb-b6c3-162acbca0d1c'; // Replace with your actual API key
+        const imageUrl = document.getElementById('ai-image-url').value;
 
         const payload = {
             object: imageUrl
         };
 
         const xhr = new XMLHttpRequest();
-        xhr.open('POST', apiUrl, true);
-        xhr.setRequestHeader('X-API-KEY', apiKey);
+        xhr.open('POST', apiUrl);
         xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('X-API-KEY', apiKey);
 
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    const data = JSON.parse(xhr.responseText);
-                    resultContainer.innerHTML = `
-                        <h3>AI Detection Result:</h3>
-                        <p>AI Confidence: ${data.report.ai.confidence}</p>
-                        <p>AI Detected: ${data.report.ai.is_detected ? 'Yes' : 'No'}</p>
-                    `;
-                } else {
-                    console.error('Error:', xhr.statusText);
-                    resultContainer.innerHTML = '<p>An error occurred. Please try again later.</p>';
-                }
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                const response = JSON.parse(xhr.responseText);
+                resultContainer.innerHTML = `
+                    <h3>AI Detection Result:</h3>
+                    <p>AI Confidence: ${response.report.ai.confidence}</p>
+                    <p>AI Detected: ${response.report.ai.is_detected ? 'Yes' : 'No'}</p>
+                `;
+                resultContainer.classList.remove('hide');
+            } else {
+                resultContainer.innerHTML = '<p>An error occurred. Please try again later.</p>';
+                resultContainer.classList.remove('hide');
             }
         };
 
